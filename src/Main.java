c893c72c1a879d07992043ab30a283d5840f202e
Madrid-7 <783798088@qq.com>
2019-10-20 22:32:34 +0800

    10_20,test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+/*
+class Rectangle {
+
+    private double width;
+    private double height;
+    Rectangle() {
+        width = 1;
+        height = 1;
+    }
+    Rectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+    public double getArea() {
+        return width * height;
+    }
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
 public class Main {
 
 /*
@@ -11,9 +31,36 @@ public class leapYear{
         }
     }
 }
-*/
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+}
+
+*/
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        int num=0,chartra=0,blak=0,other=0;
+        Scanner sc =new Scanner(System.in);
+        String result=sc.nextLine();
+        char x[]=result.toCharArray();
+        for(int i=0;i<x.length;i++) {
+            if(Character.isDigit(x[i])){
+                 num++;
+            } else if (Character.isLetter(x[i])) {
+                chartra++;
+            } else if (Character.isSpace(x[i])) {
+                blak++;
+            } else{
+                other++;
+            }
+        }
+        System.out.println("字母个数："+chartra);
+        System.out.println("数字个数："+num);
+        System.out.println("空格个数："+blak);
+        System.out.println("其他字符个数："+other);
     }
 }
---
+++ src/Test_10_20.java
@@ -0,0 +1,75 @@
+public class Test_10_20 {
+    public static int[] transForm(int[] array) {
+        int[] arr2 = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arr2[i] = array[i] * 2;
+        }
+        return arr2;
+    }
+
+    public static String toString(int[] array) {
+        String str = "[";
+        for (int i = 0; i < array.length; i++) {
+            str += array[i];
+            if (i != array.length - 1) {
+                str += ",";
+            }
+        }
+        str += "]";
+        return str;
+    }
+
+    public static int arrMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int arrMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static double arrAve(int[] arr) {
+        double sum = 0.0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum/arr.length;
+    }
+
+    public static int[] arrReverse(int[] arr) {
+        int[] array = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            array[i] = arr[arr.length - 1 - i];
+        }
+        return array;
+    }
+
+    public static int[] arrChange(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if(arr[left] % 2 == )
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] array2 = transForm(array);
+        for (int x : array2) {
+            System.out.println(x);
+        }
+        System.out.println(toString(array));
+
+    }
+}
