356a68e0d0061492bd6567098b76937ddaea7bfb
Madrid-7 <783798088@qq.com>
2020-02-01 17:57:13 +0800

    vacation_day01

---
+++ 2020_1_31/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Main a = new Main();
+        String str = "ASdfF";
+        System.out.println(a.toLowerCase(str));
+    }
+}
